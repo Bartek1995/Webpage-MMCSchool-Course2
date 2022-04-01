2a460be497b3288441e5643a47945ef60fc76571
Bartek1995 <67422578+Bartek1995@users.noreply.github.com>
2022-04-01 04:46:44 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nav = document.querySelector('#navbar')
+    const navHref = document.querySelectorAll('.nav-link')
+    const navMark = document.querySelector('#navbarNavAltMarkup')
+
+    function addShadow(){
+        if (window.scrollY >= 60){
+            nav.classList.add('shadow-bg')
+        }else{
+            nav.classList.remove('shadow-bg')
+        }
+    }
+
+
+    navHref.forEach(element => element.addEventListener('click', () =>
+        navMark.classList.remove('show')
+    ))
+
+    window.addEventListener('scroll', addShadow)
+})
+
+
